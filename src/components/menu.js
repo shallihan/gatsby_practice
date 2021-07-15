808f35ee2d898874764c8a94571e4e2a6337937d
shallihan <smhallihan@gmail.com>
2021-07-15 12:18:14 +0100

    Add Navigation Menu.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react"
+import Link from "gatsby-link"
 
-const Menu = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-};
+const Menu = () => (
+  <div
+    style={{
+      background: "#F4F4F4",
+      paddingTop: "10px",
+    }}
+  >
+    <ul
+      style={{
+        listStyle: "none",
+        display: "flex",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/services">Services</Link>
+      </li>
+    </ul>
+  </div>
+)
 
-
-export default Menu;
+export default Menu
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import "./index.css";
 
 const IndexPage = () => (
   <Layout>
